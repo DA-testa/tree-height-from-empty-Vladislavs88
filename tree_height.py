d84ebf5e3b665992028cd899c96c86badc91aff8
221RDB070 <125796943+Vladislavs88@users.noreply.github.com>
2023-03-05 20:59:16 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -44,7 +44,7 @@ def main():
                 vecaki=list(map(int,fail.readline().split()))
         else:
           print("kluda")
-    else:
+    
     print(compute_height(cipari, vecaki))    
  
         
